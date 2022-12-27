f41f554a77badd346ef833aab60a5b66ede8848d
tanishqraj360 <121513453+tanishqraj360@users.noreply.github.com>
2022-12-28 00:05:02 +0530

    Delete E4.py

--- E4.py
+++
@@ -1,8 +0,0 @@
-a=str(input('Enter string: '))
-l=['1','2','3','4','5','6','7','8','9','0']
-for i in l:
-    c=0
-    for j in a:
-        if i==j:
-            c+=1
-    print('Number of %s: '%(i),c)
