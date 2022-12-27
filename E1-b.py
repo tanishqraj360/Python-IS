3220ef07571f76ada6b93f96163e54bd29dbcf9b
tanishqraj360 <121513453+tanishqraj360@users.noreply.github.com>
2022-12-28 00:04:28 +0530

    Delete E1-b.py

--- E1-b.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-name=input('Enter name of person: ')
-yob=int(input('Enter Year of Birth of %s'%(name)))
-age=datetime.now().year-yob
-if age>=60:
-    print('%s is a senior citizen.'%(name))
-else:
-    print('%s is not a senior citizen.'%(name))
-    
