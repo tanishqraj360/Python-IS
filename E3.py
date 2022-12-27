93d2b26430bf9c8fe3b01b4c376d1aab8373b728
tanishqraj360 <121513453+tanishqraj360@users.noreply.github.com>
2022-12-28 00:04:53 +0530

    Delete E3.py

--- E3.py
+++
@@ -1,9 +0,0 @@
-import statistics
-l=[]
-n=int(input('How many numbers do you want to enter: '))
-for i in range(n):
-    a=int(input('Enter number: '))
-    l.append(a)
-print(statistics.mean(l))
-print(statistics.variance(l))
-print(statistics.stdev(l))
